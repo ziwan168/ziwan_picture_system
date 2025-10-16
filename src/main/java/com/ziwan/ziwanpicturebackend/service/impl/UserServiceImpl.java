b734cdddf1a48508d7d1bc0ce9f5696018104cca
ziwanqing <14936540+ziwanqing@user.noreply.gitee.com>
2025-10-16 21:12:04 +0800

    修改后增加空间

--- src/main/java/com/ziwan/ziwanpicturebackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/ziwan/ziwanpicturebackend/service/impl/UserServiceImpl.java
@@ -21,6 +21,7 @@ import com.ziwan.ziwanpicturebackend.service.SpaceService;
 import com.ziwan.ziwanpicturebackend.service.UserService;
 import com.ziwan.ziwanpicturebackend.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
@@ -38,6 +39,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
 
     @Resource
+    @Lazy
     private SpaceService spaceService;
 
 
